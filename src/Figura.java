5a298bbcac37088b14d40b96eff018e2898d90a0
Katecheta <62699228+Katecheta@users.noreply.github.com>
2020-04-25 09:47:55 +0200

    kolejny dzien
    to to

--- src/Figura.java
+++ src/Figura.java
@@ -1,5 +1,7 @@
 public class Figura {
 
+    String[][] poleGry = new String[10][10]; // tablica pola gry
+
     int wspX1;
     int wspY1;
 
@@ -13,7 +15,7 @@ public class Figura {
     int wspY4;
 
 
-    Figura(int x) {
+    Figura(int x) {  // Konstruktor figor
         switch (x) {
             case 1: {
                 wspX1 = 0;           // pierwszy dlugiKlocek
@@ -25,10 +27,71 @@ public class Figura {
                 wspX4 = 3;
                 wspY4 = 4;
             }
+            case 2: {
+                wspX1 = 0;           // drugi kwadrat
+                wspY1 = 4;
+                wspX2 = 1;
+                wspY2 = 4;
+                wspX3 = 0;
+                wspY3 = 5;
+                wspX4 = 1;
+                wspY4 = 5;
+            }
         }
-    }  // Konstruktor figor
-
-    public void obrutFigury() {
-
+        zerowanie();
     }
+
+    public void ruchDolu() {
+        wspX1 += 1;
+        wspX2 += 1;
+        wspX3 += 1;
+        wspX4 += 1;
+    }
+
+    public void ruchPrawo() {
+        wspY1 += 1;
+        wspY2 += 1;
+        wspY3 += 1;
+        wspY4 += 1;
+    }
+
+    public void ruchLewo() {
+        wspY1 -= 1;
+        wspY2 -= 1;
+        wspY3 -= 1;
+        wspY4 -= 1;
+    }
+
+    public nanoszenieFiguryNaPole() {
+        poleGry[wspX1][wspY1] = "[**]";
+        poleGry[wspX2][wspY2] = "[**]";
+        poleGry[wspX3][wspY3] = "[**]";
+        poleGry[wspX3][wspY3] = "[**]";
+        poleGry[wspX4][wspY4] = "[**]";
+
+        return poleGry;
+    }       //  nanoszenie figury na pole.
+
+    public void rysuj() {
+
+        System.out.println("------------------------------------------");
+
+        for (int rzad = 0; rzad < poleGry.length; rzad++) {
+            System.out.print("|");
+            for (int kolumna = 0; kolumna < poleGry.length; kolumna++) {
+                System.out.print(poleGry[rzad][kolumna]);
+            }
+            System.out.println("|");
+        }
+        System.out.println("------------------------------------------");
+    }    // metoda kotra rysuje pole gry z objektu RysujPoleGry.
+
+    public void zerowanie() {
+        for (int wiersz = 0; wiersz < poleGry.length; wiersz++) {
+            for (int kolumna = 0; kolumna < poleGry.length; kolumna++) {
+                poleGry[wiersz][kolumna] = "    ";
+            }
+        }
+    } // pierwsze wypelnienie tablicyna pusto.
+
 }
--- src/Tetris.java
+++ src/Tetris.java
@@ -2,39 +2,29 @@
 public class Tetris {
 
 
-    String[][] poleGry = new String[10][10]; // tablica pola gry
+
 
     public static void main(String[] args) {
-        //---------------------------------------Poczatek---------------------------------------------------------------------------
-        Tetris RysujPoleGry = new Tetris();// stworzenie objektu RysujPole gry classy tetris zeby nie uzywac static w funkcji rysuj
-        RysujPoleGry.pierwszeWypelnienie();
-        RysujPoleGry.rysuj();// rysowanie pola gry
+        //---------------------------------------Poczatek--------------------------------------------------------------------------
 
-        //-------------------------------------------------------------------------------------------------------------------------
 
-        Figura dlugiKlocek = new Figura(1);
+        //---------------------------------------Tu zaczniemu petle gry------------------------------------------------------------
+
+        Figura klocek = new Figura(1);  // tworzymy nowy klocek.
+        klocek.zerowanie(); // czysci pole gry do stanu wyjsciowego
+        klocek.nanoszenieFiguryNaPole()
+        klocek.rysuj();
+
+
     }
 
 
-    private void rysuj() {
 
-        System.out.println("------------------------------------------");
 
-        for (int rzad = 0; rzad < poleGry.length; rzad++) {
-            System.out.print("|");
-            for (int kolumna = 0; kolumna < poleGry.length; kolumna++) {
-                System.out.print(poleGry[rzad][kolumna]);
-            }
-            System.out.println("|");
-        }
-        System.out.println("------------------------------------------");
-    }    // metoda kotra rysuje pole gry z objektu RysujPoleGry.
-    private  void pierwszeWypelnienie(){
-        for(int wiersz = 0 ; wiersz < poleGry.length ; wiersz++){
-            for(int kolumna = 0 ; kolumna < poleGry.length ; kolumna++ ){
-                poleGry[wiersz][kolumna] = "    ";
-            }
-        }
-    } // pierwsze wypelnienie tablicyna pusto.
+
+
+
+
+
 
 }
